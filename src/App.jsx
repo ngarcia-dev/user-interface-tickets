5c62fe72fa445e9d919790763c0f66d007cec29d
Nahuel <68356271+ngarcia-dev@users.noreply.github.com>
2024-04-08 18:44:10 -0300

    Update dependencies and fix styling issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
@@ -16,41 +18,46 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <AuthProvider>
-      <TicketProvider>
-        <DependencyProvider>
-          <BrowserRouter>
-            <main className="container mx-auto px-10">
-              <Navbar />
-              <Routes>
-                <Route path="/" element={<LoginPage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
+    <Theme appearance="dark">
+      <AuthProvider>
+        <TicketProvider>
+          <DependencyProvider>
+            <BrowserRouter>
+              <main className="container mx-auto px-10">
+                <Navbar />
+                <Routes>
+                  <Route path="/" element={<LoginPage />} />
+                  <Route path="/login" element={<LoginPage />} />
+                  <Route path="/register" element={<RegisterPage />} />
 
-                <Route path="*" element={<NotFoundPage />} />
-                <Route element={<ProtectedRoute />}>
-                  <Route path="/tickets" element={<TicketsPage />} />
-                  <Route path="/add-ticket" element={<TicketsFormPage />} />
-                  <Route path="/tickets/:id" element={<TicketsFormPage />} />
+                  <Route path="*" element={<NotFoundPage />} />
+                  <Route element={<ProtectedRoute />}>
+                    <Route path="/tickets" element={<TicketsPage />} />
+                    <Route path="/add-ticket" element={<TicketsFormPage />} />
+                    <Route path="/tickets/:id" element={<TicketsFormPage />} />
 
-                  <Route path="/dependencies" element={<DependenciesPage />} />
-                  <Route
-                    path="/add-dependency"
-                    element={<DependenciesFormPage />}
-                  />
-                  <Route
-                    path="/dependencies/:id"
-                    element={<DependenciesFormPage />}
-                  />
+                    <Route
+                      path="/dependencies"
+                      element={<DependenciesPage />}
+                    />
+                    <Route
+                      path="/add-dependency"
+                      element={<DependenciesFormPage />}
+                    />
+                    <Route
+                      path="/dependencies/:id"
+                      element={<DependenciesFormPage />}
+                    />
 
-                  <Route path="/profile" element={<ProfilePage />} />
-                </Route>
-              </Routes>
-            </main>
-          </BrowserRouter>
-        </DependencyProvider>
-      </TicketProvider>
-    </AuthProvider>
+                    <Route path="/profile" element={<ProfilePage />} />
+                  </Route>
+                </Routes>
+              </main>
+            </BrowserRouter>
+          </DependencyProvider>
+        </TicketProvider>
+      </AuthProvider>
+    </Theme>
   );
 }
 
