10cee91e2cdde83f7de8575f0b287dc4c845da13
ngarcia <garcianahuel77@gmail.com>
2024-04-09 13:01:15 -0300

    Update Navbar component styling and dependencies

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,55 +1,86 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
+import { Flex, Box, Button, Badge, Heading, Grid } from "@radix-ui/themes";
+import { ExitIcon, AvatarIcon } from "@radix-ui/react-icons";
+
 function Navbar() {
   const { isAuthenticated, logout, user } = useAuth();
 
   return (
-    <nav className="bg-zinc-700 my-3 flex justify-between py-5 px-10 rounded-lg">
-      <Link
-        className="flex items-center"
-        to={isAuthenticated ? "/tickets" : "/login"}
+    <nav>
+      <Flex
+        my="4"
+        px="5"
+        align="center"
+        justify="between"
+        style={{
+          backgroundColor: "var(--gray-a2)",
+          borderRadius: "var(--radius-3)",
+        }}
       >
-        <h1 className="text-2xl font-bold item">Ticket Manager</h1>
-      </Link>
-      <ul className="flex gap-x-2 flex-wrap gap-3 m-3">
-        {isAuthenticated ? (
-          <>
-            <li>
-              <Link to="/profile">Welcome {user.username}</Link>
-            </li>
-            <li>
-              <Link
-                to="/dependencies"
-                className="bg-indigo-500 px-2 py-1 rounded-sm"
-              >
-                Dependencias
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/add-ticket"
-                className="bg-indigo-500 px-2 py-1 rounded-sm"
-              >
-                Add Ticket
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/login"
-                className="bg-red-500 px-1 py-1 rounded-sm"
-                onClick={() => {
-                  logout();
-                }}
-              >
-                Logout
-              </Link>
-            </li>
-          </>
-        ) : (
-          <></>
-        )}
-      </ul>
+        <Link to={isAuthenticated ? "/tickets" : "/login"}>
+          <Heading>Ticket Manager</Heading>
+        </Link>
+        <Grid columns={{ initial: "1" }} width="auto">
+          <Flex gap="4" my="6" wrap="wrap">
+            {isAuthenticated ? (
+              <>
+                <Flex align="center">
+                  <Box>
+                    <Link to="/profile">
+                      <Badge
+                        className="hover:cursor-pointer"
+                        size={{ initial: "1", sm: "3" }}
+                      >
+                        <AvatarIcon /> {user.username}
+                      </Badge>
+                    </Link>
+                  </Box>
+                </Flex>
+                <Box>
+                  <Link to="/dependencies">
+                    <Button
+                      className="hover:cursor-pointer"
+                      size={{ initial: "1", sm: "2" }}
+                    >
+                      Dependencies
+                    </Button>
+                  </Link>
+                </Box>
+                <Box>
+                  <Link to="/add-ticket">
+                    <Button
+                      className="hover:cursor-pointer"
+                      size={{ initial: "1", sm: "2" }}
+                    >
+                      Add Ticket
+                    </Button>
+                  </Link>
+                </Box>
+                <Box>
+                  <Link
+                    to="/"
+                    onClick={() => {
+                      logout();
+                    }}
+                  >
+                    <Button
+                      className="hover:cursor-pointer"
+                      color="red"
+                      size={{ initial: "1", sm: "2" }}
+                    >
+                      <ExitIcon />
+                    </Button>
+                  </Link>
+                </Box>
+              </>
+            ) : (
+              <></>
+            )}
+          </Flex>
+        </Grid>
+      </Flex>
     </nav>
   );
 }
