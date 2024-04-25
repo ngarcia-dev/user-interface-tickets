b554f9e7dc489bd912196ed7e4e305242bfdea29
ngarcia <garcianahuel77@gmail.com>
2024-04-25 14:17:22 -0300

    Update axios.js to use import.meta.env.VITE_API_URL for baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
-import { API_URL } from "../config.js";
 
 const instance = axios.create({
-  baseURL: API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
